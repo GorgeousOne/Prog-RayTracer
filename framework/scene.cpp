25826d3e2752955334373748bb575ac7302e32a8
mathis0305 <kuenzelmathis@gmx.de>
2021-07-07 13:47:59 +0200

    add scene functions

---
+++ framework/scene.cpp
@@ -0,0 +1,20 @@
+#include "scene.hpp"
+
+Scene::Scene() :
+	shapes{ {} },
+	lights{ {} },
+	ambient{ {} },
+	camera{ {} },
+	pixel_array{ {} } {}
+
+std::vector<Pixel> create_image(int res_x, int res_y) {
+	return {};
+}
+
+void add_shape(Shape const& shape) {
+
+}
+
+void remove_shape(Shape const& shape) {
+
+}
--- framework/scene.hpp
+++ framework/scene.hpp
@@ -1,10 +1,15 @@
 #ifndef RAYTRACER_SCENE_HPP
 #define RAYTRACER_SCENE_HPP
 
+#include "shape.hpp"
 #include <vector>
 
 
-struct Scene {
+class Scene {
+public:
+	Scene();
+
+private:
 	std::vector<Shape> shapes;
 	std::vector<PointLight> lights;
 	AmbientLight ambient;
@@ -12,5 +17,8 @@ struct Scene {
 	std::vector<Pixel> pixel_array;
 };
 
+std::vector<Pixel> create_image(int res_x, int res_y);
+void add_shape(Shape const& shape);
+void remove_shape(Shape const& shape);
 
 #endif
