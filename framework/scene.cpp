6d0a1a002d2a34bba976db19349accce5c937c6a
IEatSushi <2klaus.4peter@gmail.com>
2021-07-07 14:28:18 +0200

    WIP load scene function

--- framework/scene.cpp
+++ framework/scene.cpp
@@ -1,21 +1,30 @@
+#include <fstream>
+#include <sstream>
 #include "scene.hpp"
 
-Scene::Scene() :
-	shapes{ {} },
-	lights{ {} },
-	ambient{ {} },
-	camera{ {} } {}
-	//pixel_array{ {} }
+Scene loadScene(std::string const& file_path) {
+	Scene new_scene{};
 
-/*std::vector<Pixel> create_image(int res_x, int res_y) {
-	return {};
-}
-*/
+	std::ifstream input_sdf_file(file_path);
+	std::string line_buffer = "";
 
-void add_shape(Shape const& shape) {
+	//writes sdf file line by line into line_buffer
+	while (std::getline(input_sdf_file, line_buffer)) {
+		std::cout << line_buffer << std::endl;
+		std::istringstream words_stream(line_buffer);
 
-}
+		//streams words of each line
+		std::string token_str = "";
+		words_stream >> token_str;
 
-void remove_shape(Shape const& shape) {
+		if ("#" == token_str) {
+			continue;
+		}
+		if ("define" == token_str) {
 
+		} else if ("render") {
+
+		}
+	}
+	return new_scene;
 }
--- framework/scene.hpp
+++ framework/scene.hpp
@@ -1,28 +1,17 @@
 #ifndef RAYTRACER_SCENE_HPP
 #define RAYTRACER_SCENE_HPP
 
-#include "shape.hpp"
+#include "Shape.hpp"
 #include "camera.hpp"
 #include "pointLight.hpp"
 #include "ambientLight.hpp"
 #include <vector>
 #include <map>
 
-
-class Scene {
-public:
-	Scene();
-
-private:
-	std::map<std::string, std::shared_ptr<Shape>> shapes;
-	std::vector<PointLight> lights;
-	AmbientLight ambient;
-	Camera camera;
-	//std::vector<Pixel> pixel_array;
+struct Scene {
+	std::map<std::string, std::shared_ptr<Shape>> shapes{};
+	std::vector<PointLight> lights{};
+	AmbientLight ambient{};
+	Camera camera{};
 };
-
-//std::vector<Pixel> create_image(int res_x, int res_y);
-void add_shape(Shape const& shape);
-void remove_shape(Shape const& shape);
-
 #endif
